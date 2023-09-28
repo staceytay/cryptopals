360bf6b8f5f4b7232c059ad4249a689efa87fdc5
staceytay <staceytay@users.noreply.github.com>
2023-09-28 12:40:41 -0500

    Set 1 Challenge 3

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
+use std::collections::HashMap;
 use std::num::ParseIntError;
 
 fn main() {
-    let hex = String::from("1c0111001f010100061a024b53535009181c");
-    let buf = String::from("686974207468652062756c6c277320657965");
+    let hex = String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+    let bts = decode_hex(&hex).unwrap();
+    let mut counts = HashMap::new();
+    for b in bts.iter() {
+        *counts.entry(b).or_insert(0) += 1;
+    }
+    println!("{:?}", counts);
+
+    let key = 120 ^ 32;
     println!(
-        "{:02X?}",
-        fixed_xor(&decode_hex(&hex).unwrap(), &decode_hex(&buf).unwrap())
+        "{}",
+        show(
+            &bts.clone()
+                .into_iter()
+                .map(|b| b ^ key)
+                .collect::<Vec<u8>>()[..]
+        )
     );
+
+    for i in 32..126 {
+        let key = 120 ^ i;
+        println!(
+            "{}: {:?}",
+            i,
+            show(
+                &bts.clone()
+                    .into_iter()
+                    .map(|b| b ^ key)
+                    .collect::<Vec<u8>>()[..]
+            )
+        );
+    }
 }
 
 // From https://stackoverflow.com/questions/52987181/how-can-i-convert-a-hex-string-to-a-u8-slice.
@@ -22,4 +49,14 @@ fn fixed_xor(b1: &[u8], b2: &[u8]) -> Vec<u8> {
         .zip(b2.into_iter())
         .map(|(u1, u2)| u1 ^ u2)
         .collect()
+}
+
+// From https://stackoverflow.com/questions/41449708/how-to-print-a-u8-slice-as-text-if-i-dont-care-about-the-particular-encoding.
+fn show(bs: &[u8]) -> String {
+    let mut visible = String::new();
+    for &b in bs {
+        let part: Vec<u8> = std::ascii::escape_default(b).collect();
+        visible.push_str(std::str::from_utf8(&part).unwrap());
+    }
+    visible
 }
