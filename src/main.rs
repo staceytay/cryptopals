3213f31e624ec3a2661340ccb35c458555112501
staceytay <staceytay@users.noreply.github.com>
2023-10-13 13:43:55 -0500

    Set 1 Challenge 8

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,42 @@
-use base64::{engine::general_purpose, Engine as _};
-use openssl::symm::{decrypt, Cipher};
+use std::num::ParseIntError;
 
-const FILE_7: &'static str = include_str!("../7.txt");
 const FILE_8: &'static str = include_str!("../8.txt");
 
 fn main() {
-    let song = general_purpose::STANDARD
-        .decode(FILE_7.replace("\n", ""))
-        .unwrap();
-
-    let mut min: f64 = 100.0;
+    let mut results = Vec::new();
     for (linum, line) in FILE_8.split("\n").enumerate() {
-        let bytes = general_purpose::STANDARD.decode(line).unwrap();
+        let bytes = decode_hex(line).unwrap();
 
+        // Compare each block in the ciphertext with each other and compute
+        // their average edit distance.
         let block_size = 16;
         let num_blocks = bytes.len() / block_size;
         let mut sum = 0;
         for i in 0..num_blocks {
-            for j in 0..(song.len() / block_size) {
+            for j in (i + 1)..num_blocks {
                 sum += edit_distance(
                     &bytes[((i + 0) * block_size)..((i + 1) * block_size)],
-                    &song[((j + 0) * block_size)..((j + 1) * block_size)],
+                    &bytes[((j + 0) * block_size)..((j + 1) * block_size)],
                 );
             }
         }
 
-        let distance = sum as f64 / ((num_blocks * (song.len() / block_size)) as f64);
-        println!("LINE = {:02}: {:.3}, {sum}", linum, distance);
-        min = min.min(distance);
-        // // println!("Edit distance (avg) comparing {comparisons} blocks");
-        // let mut sum = 0.0;
-
-        // for j in 0..comparisons {
-        //     sum += f64::from(edit_distance(
-        //         &bytes[((j + 0) * block_size)..((j + 1) * block_size)],
-        //         &bytes[((j + 1) * block_size)..((j + 2) * block_size)],
-        //     )) / block_size as f64;
-        // }
-        // let distance = sum / comparisons as f64;
-
-        // println!("KEYSIZE = {:02}: {:.3}", block_size, distance);
-        // println!("");
-
-        // let cipher = Cipher::aes_128_ecb();
-        // if let Ok(message) = decrypt(cipher, KEY.as_bytes(), None, &bytes) {
-        //     println!("message: {}", String::from_utf8(message).unwrap());
-        // }
+        let distance = sum as f64 / ((120) as f64);
+        results.push((linum, distance));
     }
-    println!("MIN: {}", min);
+    results.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+
+    for (linum, distance) in results {
+        println!("LINE = {:03}: {:.3}", linum, distance);
+    }
+}
+
+// From https://stackoverflow.com/questions/52987181/how-can-i-convert-a-hex-string-to-a-u8-slice.
+fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
 }
 
 fn edit_distance(b1: &[u8], b2: &[u8]) -> u32 {
