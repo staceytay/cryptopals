09c8e227d693b5c51d2ddce2a8ee6b9645feef3e
staceytay <staceytay@users.noreply.github.com>
2023-10-20 11:26:44 -0500

    Set 2 Challenge 9

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-use aes::cipher::{
-    generic_array::{typenum::U16, GenericArray},
-    BlockDecrypt, KeyInit,
-};
-use aes::Aes128;
-use base64::{engine::general_purpose, Engine as _};
-
-const FILE: &'static str = include_str!("../7.txt");
-const KEY: &'static [u8; 16] = b"YELLOW SUBMARINE";
-
 fn main() {
-    let bytes = general_purpose::STANDARD
-        .decode(FILE.replace('\n', ""))
-        .unwrap();
+    assert_eq!(
+        pkcs7pad(b"YELLOW SUBMARINE", 20),
+        b"YELLOW SUBMARINE\x04\x04\x04\x04"
+    );
+}
 
-    let key = GenericArray::from(*KEY);
-    let cipher = Aes128::new(&key);
-
-    let block_size = 16;
-    let mut message = Vec::new();
-    for i in 0..(bytes.len() / block_size) {
-        let mut block = GenericArray::<u8, U16>::clone_from_slice(
-            &bytes[(i * block_size)..((i + 1) * block_size)],
-        );
-        cipher.decrypt_block(&mut block);
-        message.extend(block);
-    }
-
-    println!("{:-^64}", "MESSAGE");
-    println!("{}", String::from_utf8(message).unwrap());
+fn pkcs7pad(block: &[u8], length: usize) -> Vec<u8> {
+    let mut padded = Vec::from(block);
+    padded.resize(length, (length - block.len()) as u8);
+    padded
 }
