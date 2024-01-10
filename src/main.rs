9364b4326c3839fb003e13d785053c7f45267afa
staceytay <staceytay@users.noreply.github.com>
2024-01-10 15:08:50 -0600

    Set 2 Challenge 12

--- src/main.rs
+++ src/main.rs
@@ -93,36 +93,6 @@ fn ecb_encrypt(bytes: &[u8], key: &[u8]) -> Vec<u8> {
     ciphertext
 }
 
-fn cbc_encrypt(bytes: &[u8]) -> Vec<u8> {
-    let key = generate_random_bytes(16);
-    let key = GenericArray::<u8, U16>::clone_from_slice(&key);
-    let cipher = Aes128::new(&key);
-
-    let block_size = 16;
-    let iv = generate_random_bytes(block_size);
-    let mut ciphertext = Vec::new();
-    let mut previous_block = iv;
-
-    let mut iter = bytes.chunks(block_size).peekable();
-    while let Some(chunk) = iter.next() {
-        if chunk.len() < block_size {
-            let chunk = pkcs7pad(chunk, block_size);
-            let mut block = GenericArray::<u8, U16>::clone_from_slice(&chunk);
-            cipher.encrypt_block(&mut block);
-            let ciphertext_block = fixed_xor(&previous_block, &block);
-            previous_block = ciphertext_block.clone();
-            ciphertext.extend(ciphertext_block);
-        } else {
-            let mut block = GenericArray::<u8, U16>::clone_from_slice(&chunk);
-            cipher.encrypt_block(&mut block);
-            let ciphertext_block = fixed_xor(&previous_block, &block);
-            previous_block = ciphertext_block.clone();
-            ciphertext.extend(ciphertext_block);
-        }
-    }
-    ciphertext
-}
-
 fn find_block_size(key: &[u8]) -> Option<usize> {
     let mut block_size = None;
     let mut last_length = None;
@@ -150,27 +120,8 @@ fn generate_random_bytes(length: usize) -> Vec<u8> {
     v
 }
 
-fn fixed_xor(b1: &[u8], b2: &[u8]) -> Vec<u8> {
-    b1.into_iter()
-        .zip(b2.into_iter())
-        .map(|(u1, u2)| u1 ^ u2)
-        .collect()
-}
-
 fn pkcs7pad(block: &[u8], length: usize) -> Vec<u8> {
     let mut padded = Vec::from(block);
     padded.resize(length, (length - block.len()) as u8);
     padded
-}
-
-fn edit_distance(b1: &[u8], b2: &[u8]) -> u32 {
-    let mut distance = 0;
-    for i in 0..b1.len() {
-        for j in 0..u8::BITS {
-            if (b1[i] >> j ^ b2[i] >> j) & 1 as u8 == 0b________1 {
-                distance += 1;
-            }
-        }
-    }
-    distance
 }
