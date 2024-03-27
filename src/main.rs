8aa017b6f301c7ed4a3b0e535afd031b14cfb853
staceytay <staceytay@users.noreply.github.com>
2024-03-27 13:29:24 -0500

    Working mess

--- src/main.rs
+++ src/main.rs
@@ -9,71 +9,127 @@ const KEY_LENGTH: usize = 16;
 
 fn main() {
     let oracle = Oracle::new();
+    let (ciphertext, iv) = oracle.encrypt();
+    // println!("{:?}", ciphertext);
+    // println!("{:?}", oracle.decrypt(&ciphertext[0..32], &iv));
 
-    // Assert that encrypt correctly escapes the `;` and `=` characters.
-    assert!(!oracle.decrypt(&oracle.encrypt(String::from(";admin=true;"))));
-
-    // We skip calculating the block size and prefix length here as it's been
-    // done in challenge 14.
     let block_size = 16;
-    let prefix_length = 32;
-    let mut ciphertext = oracle.encrypt(String::from("0".repeat(block_size * 2)));
-    // Attempt to recreate the block right after the decryption step, but before
-    // the XOR step, in CBC decryption.
-    let block1 = fixed_xor(
-        &ciphertext[prefix_length..(prefix_length + block_size)],
-        &b"0".repeat(block_size),
-    );
-    // XOR block1 with the desired text and use this as a cipher block. Then
-    // during CBC decryption, this block will produce the desired plaintext
-    // after being XOR-ed with the neighbouring decrypted block.
-    let block2 = fixed_xor(b"0000;admin=true;", &block1);
-    for i in 0..block_size {
-        ciphertext[prefix_length + i] = block2[i];
+    let mut plaintext: Vec<u8> = Vec::new();
+    for block_index in 0..(ciphertext.len() / block_size) {
+        // for block_index in 0..1 {
+        let mut intermediate_block = vec![0; block_size];
+        for i in 1..=block_size {
+            // println!("LOOP bi = {block_index}, i = {i}");
+            let mut tampered_block = vec![0; block_size];
+            for j in 1..=i {
+                tampered_block[block_size - j] = i as u8;
+            }
+            // TODO: how to prepare tempered block here to be a zeroing IV?
+
+            // println!(
+            //     "INTERMEDIATE BLOCK: {block_index}: {i}: {:?}",
+            //     intermediate_block
+            // );
+            // println!("TAMPERED BLOCK: {:?}", tampered_block);
+            tampered_block = fixed_xor(&tampered_block, &intermediate_block);
+            // println!("TAMPERED BLOCK after XOR: {:?}", tampered_block);
+            for code in u8::MIN..=u8::MAX {
+                tampered_block[block_size - i] = code;
+                // println!("TAMPERED BLOCK with code = {code}: {:?}", tampered_block);
+                let valid = oracle.decrypt(
+                    &ciphertext[block_index * block_size..(block_index + 1) * block_size],
+                    &tampered_block,
+                );
+                if valid {
+                    // println!("VALID FIRST bi = {block_index}, i = {i}, code = {code}");
+                    if i == 1 {
+                        tampered_block[block_size - i - 1] = 22u8;
+                        let valid2 = oracle.decrypt(
+                            &ciphertext[block_index * block_size..(block_index + 1) * block_size],
+                            &tampered_block,
+                        );
+                        if valid2 {
+                            // println!("VALID SECOND bi = {block_index}, i = {i}, code = {code}");
+                            // let intermediate_code =
+                            //     i as u8 ^ ciphertext[(block_index * block_size) + (block_size - i)];
+                            intermediate_block[block_size - i] = code ^ i as u8;
+                            break;
+                        }
+                        tampered_block[block_size - i - 1] = 0u8;
+                    } else {
+                        // let intermediate_code =
+                        //     i as u8 ^ ciphertext[(block_index * block_size) + (block_size - i)];
+                        intermediate_block[block_size - i] = code ^ i as u8;
+                        break;
+                    }
+                    // println!(
+                    //     "INTERMEDIATE BLOCK: {block_index}: {i}: {:?}",
+                    //     intermediate_block
+                    // );
+                    // println!("{:-^64}", "END");
+                }
+            }
+            // println!(
+            //     "INTERMEDIATE BLOCK: {block_index}: {i}: {:?}",
+            //     intermediate_block
+            // );
+        }
+        let previous_ciphertext_block = if block_index == 0 {
+            &iv
+        } else {
+            &ciphertext[(block_index - 1) * block_size..block_index * block_size]
+        };
+        let plaintext_block = fixed_xor(previous_ciphertext_block, &intermediate_block);
+        println!("Plaintext BLOCK: {:?}", plaintext_block);
+        plaintext.extend(plaintext_block);
     }
-    assert!(oracle.decrypt(&ciphertext));
+
+    println!("{:-^64}", "CIPHERTEXT");
+    println!("{:?}", ciphertext);
+    println!("{:-^64}", "PLAINTEXT");
+    println!("{:?}", plaintext);
 }
 
 struct Oracle {
-    iv: Vec<u8>,
     key: Vec<u8>,
 }
 
 impl Oracle {
     fn new() -> Oracle {
         Oracle {
-            iv: generate_random_bytes(KEY_LENGTH),
             key: generate_random_bytes(KEY_LENGTH),
         }
     }
 
-    fn encrypt(&self, input: String) -> Vec<u8> {
-        let escaped_input = [
-            "comment1=cooking%20MCs;userdata=",
-            &input.replace(";", "\\;").replace("=", "\\="),
-            ";comment2=%20like%20a%20pound%20of%20bacon",
-        ]
-        .concat()
-        .as_bytes()
-        .to_vec();
+    fn encrypt(&self) -> (Vec<u8>, Vec<u8>) {
+        let strings = [
+            "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
+            "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
+            "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
+            "MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
+            "MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
+            "MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
+            "MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
+            "MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
+            "MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
+            "MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93",
+        ];
+        let selected_string = strings[rand::thread_rng().gen_range(0..strings.len())];
+        let iv = generate_random_bytes(KEY_LENGTH);
+        println!("{:-^64}", "SELECTED STRING");
+        println!("{:?}", selected_string.as_bytes());
+        println!("{:-^64}", "END");
 
-        let ciphertext = cbc_encrypt(&self.iv, &self.key, &escaped_input);
-        ciphertext
+        let ciphertext = cbc_encrypt(&iv, &self.key, selected_string.as_bytes());
+        (ciphertext, iv)
     }
 
-    fn decrypt(&self, ciphertext: &[u8]) -> bool {
-        let message = cbc_decrypt(&self.iv, &self.key, ciphertext);
-        bytes_contain(&message, ";admin=true;".as_bytes())
+    fn decrypt(&self, ciphertext: &[u8], iv: &[u8]) -> bool {
+        let message = cbc_decrypt(iv, &self.key, ciphertext);
+        // println!("{:-^64}", "DECRYPTED MESSAGE");
+        // println!("{:?}", message);
+        validate_pkcs7pad(&message).is_ok()
     }
-}
-
-// Modified from
-// https://stackoverflow.com/questions/35901547/how-can-i-find-a-subsequence-in-a-u8-slice.
-fn bytes_contain(haystack: &[u8], needle: &[u8]) -> bool {
-    haystack
-        .windows(needle.len())
-        .position(|window| window == needle)
-        .is_some()
 }
 
 fn generate_random_bytes(length: usize) -> Vec<u8> {
@@ -141,4 +197,17 @@ fn pkcs7pad(block: &[u8], length: usize) -> Vec<u8> {
     let mut padded = Vec::from(block);
     padded.resize(length, (length - block.len()) as u8);
     padded
+}
+
+fn validate_pkcs7pad(plaintext: &[u8]) -> Result<(), &'static str> {
+    let padding_length = *plaintext.last().unwrap();
+    if padding_length == 0 {
+        return Err("invalid padding character");
+    }
+    for i in 1..=padding_length {
+        if plaintext[plaintext.len() - i as usize] != padding_length {
+            return Err("invalid padding character");
+        }
+    }
+    Ok(())
 }
