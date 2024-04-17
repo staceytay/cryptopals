4b99af2a9c2fc96da658ce9cbdf2aa994b62f36a
staceytay <staceytay@users.noreply.github.com>
2024-04-17 14:21:56 -0500

    Set 3 Challenge 23

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
-use rand::Rng;
-use std::thread;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
-
 const N: usize = 624;
 const M: usize = 397;
 
 fn main() {
-    thread::sleep(Duration::from_secs(rand::thread_rng().gen_range(40..=1000)));
+    let mut rand0 = MT19937::new(rand::random::<u32>());
 
-    let seed = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-    let mut rand = MT19937::new(seed as u32);
-    let output = rand.gen();
+    // Clone rand0's state.
+    let mut state = [0; N];
+    for i in 0..N {
+        state[i] = untemper(rand0.gen());
+    }
+    let mut rand1 = MT19937::splice(state);
 
-    thread::sleep(Duration::from_secs(rand::thread_rng().gen_range(40..=1000)));
+    // Check that the spliced generator rand1 predicts the output of rand0.
+    for _ in 0..4096 {
+        assert_eq!(rand0.gen(), rand1.gen());
+    }
+}
 
-    let mut guess = None;
-    let unix_time = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-    for i in 40..=1000 {
-        let seed = unix_time as u32 - i;
-        let mut rand = MT19937::new(seed);
-        if rand.gen() == output {
-            guess = Some(seed);
-            break;
+// Referenced from
+// https://gist.github.com/Rhomboid/b1a882c70b7a1901efa9#file-mersenne_predict-py-L77.
+fn untemper(x: u32) -> u32 {
+    fn undo_xor_rshift(x: u32, shift: usize) -> u32 {
+        // Reverses the operation x ^= (x >> shift).
+        let mut result = x;
+        for shift_amount in (shift..32).step_by(shift) {
+            result ^= x >> shift_amount;
         }
+        result
     }
 
-    match guess {
-        None => println!("Seed not found"),
-        Some(x) => println!("Seed found: {x}"),
+    fn undo_xor_lshiftmask(mut x: u32, shift: usize, mask: u32) -> u32 {
+        // Reverses the operation x ^= ((x << shift) & mask).
+        let mut window = (1 << shift) - 1;
+        for _ in 0..(32 / shift) {
+            x ^= ((window & x) << shift) & mask;
+            window <<= shift;
+        }
+        x
     }
+
+    let mut y = x;
+    y = undo_xor_rshift(y, 18);
+    y = undo_xor_lshiftmask(y, 15, 4022730752);
+    y = undo_xor_lshiftmask(y, 7, 2636928640);
+    y = undo_xor_rshift(y, 11);
+    y
 }
 
 struct MT19937 {
@@ -50,6 +60,10 @@ impl MT19937 {
             state[i] = (1812433253 * (state[i - 1] ^ state[i - 1] >> 30) as u64 + i as u64) as u32;
         }
 
+        MT19937 { index: N, state }
+    }
+
+    fn splice(state: [u32; N]) -> MT19937 {
         MT19937 { index: N, state }
     }
 
